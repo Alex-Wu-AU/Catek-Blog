1aa0d22a9a385b1c83e2e168d4254013d751c8f8
Alex-Wu-AU <alex.nanwu@gmail.com>
2023-10-02 14:45:06 +1100

    BlogPost component page layout

--- personal-blog-app/src/views/Home.vue
+++ personal-blog-app/src/views/Home.vue
@@ -29,12 +29,36 @@ export default {
           blogHTML: "a",
           blogPost:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          blogCoverPhoto: "beautiful-stories",
         },
         {
           title: "Blog Post 2",
           blogHTML: "a",
           blogPost:
             "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+      sampleBlogCrds: [
+        {
+          blogTitle: "Blog Post 1",
+          blogCoverPhoto: "stock-1",
+          blogDate: "Sept 1, 2023",
+        },
+        {
+          blogTitle: "Blog Post 2",
+          blogCoverPhoto: "stock-2",
+          blogDate: "Sept 1, 2023",
+        },
+        {
+          blogTitle: "Blog Post 3",
+          blogCoverPhoto: "stock-3",
+          blogDate: "Sept 1, 2023",
+        },
+        {
+          blogTitle: "Blog Post 4",
+          blogCoverPhoto: "stock-4",
+          blogDate: "Sept 1, 2023",
         },
       ],
     };
